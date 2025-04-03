8e15fd7e0eb6fecdada1531094c8da67350ece4c
Pascal Jahan Elahi <pascaljelahi@gmail.com>
2025-04-03 22:37:12 +0800

    Update to tests
    commented out some tests related to custom serializer since might not be
    necessary. Added some tests to check multi-vqpu and circuit submisison

--- workflow/tests/test_serialization.py
+++ workflow/tests/test_serialization.py
@@ -8,11 +8,10 @@ This workflow spins up two or more vqpus and then has a workflow that runs cpu/g
 import io
 import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(__file__))+'/../')
-#sys.path.append(os.path.dirname(os.path.abspath(__file__))+'/../vqpucommon/')
 import json
-# from vqpucommon.vqpuworkflow import HybridQuantumWorkflowBase, HybridQuantumWorkflowSerializer, launch_vqpu_workflow, cpu_workflow, SillyTestClass, TaskForSillyTestClass, FlowForSillyTestClass
-from vqpucommon.vqpubase import HybridQuantumWorkflowBase, HybridQuantumWorkflowSerializer, SillyTestClass
-from vqpucommon.vqpuflow import launch_vqpu_workflow, cpu_workflow, FlowForSillyTestClass
+from vqpucommon.vqpubase import HybridQuantumWorkflowBase, SillyTestClass
+#from vqpucommon.vqpubase import HybridQuantumWorkflowSerializer
+from vqpucommon.vqpuflow import launch_vqpu_workflow, cpu_workflow, FlowForSillyTestClass, circuits_vqpu_workflow
 from vqpucommon.utils import EventFile
 from vqpucommon.clusters import get_dask_runners
 from circuits.qristal_circuits import simulator_setup, noisy_circuit
@@ -43,6 +42,45 @@ def SillyFlow(baseobj : SillyClass | None = None):
     future = SillyTask.submit(obj1 = obj1, obj2 = obj2)
     future.result()
 
+@flow(name = "Multi-vQPU Test", 
+    description = "Running a multi-(v)QPU+CPU+GPU hybrid workflow", 
+    retries = 3, retry_delay_seconds = 10, 
+    log_prints=True, 
+    )
+async def multivqpuworkflow(
+    myqpuworkflow : HybridQuantumWorkflowBase, 
+    circuitargs : str,  
+    ):
+    circflow = circuits_vqpu_workflow.with_options(
+                    task_runner = myqpuworkflow.gettaskrunner('circuit'),
+                )
+    vqpuflows = dict()
+    circuits = dict()
+    for vqpu_id in myqpuworkflow.vqpu_ids:
+        vqpuflows[f'vqpu_{vqpu_id}'] = launch_vqpu_workflow.with_options(
+            task_runner = myqpuworkflow.gettaskrunner('vqpu'),
+            )
+    for vqpu_id in myqpuworkflow.vqpu_ids:
+        circuits[f'vqpu_{vqpu_id}'] = [noisy_circuit, noisy_circuit, noisy_circuit]
+
+    async with asyncio.TaskGroup() as tg:
+        for vqpu_id in myqpuworkflow.vqpu_ids:
+            tg.create_task(
+                vqpuflows[f'vqpu_{vqpu_id}'](
+                    myqpuworkflow = myqpuworkflow, 
+                    walltime = 200,  
+                    vqpu_id = vqpu_id, 
+                    ))
+        for vqpu_id in myqpuworkflow.vqpu_ids:
+            tg.create_task(
+                circflow(
+                    myqpuworkflow = myqpuworkflow, 
+                    vqpu_id=vqpu_id,
+                    circuits = circuits[f'vqpu_{vqpu_id}'], 
+                    arguments = circuitargs, 
+                    circuits_complete = True, 
+                    ))
+
 
 class TestCustomSerializer(unittest.TestCase):
 
@@ -66,55 +104,55 @@ class TestCustomSerializer(unittest.TestCase):
         print('Check if json serialization of object works')
         self.assertEqual(myflow, loaded_object)
 
-    def test_serialize(self):
-        frame = inspect.currentframe()
-        # Get the function name
-        function_name = frame.f_code.co_name
-        # Get the line number
-        line_number = frame.f_lineno
-        print(f"Function name: {function_name}, Line number: {line_number}")
-        serializer = HybridQuantumWorkflowSerializer()
-        myflow = HybridQuantumWorkflowBase(
-            cluster = 'ella-qb', 
-            vqpu_ids = [1, 2, 3, 16], 
-        )
-        serialized = serializer.serialize(myflow)
-        print('Check if serialization using custom serializer works')
-        self.assertEqual(serialized, myflow.to_dict())
+    # def test_serialize(self):
+    #     frame = inspect.currentframe()
+    #     # Get the function name
+    #     function_name = frame.f_code.co_name
+    #     # Get the line number
+    #     line_number = frame.f_lineno
+    #     print(f"Function name: {function_name}, Line number: {line_number}")
+    #     serializer = HybridQuantumWorkflowSerializer()
+    #     myflow = HybridQuantumWorkflowBase(
+    #         cluster = 'ella-qb', 
+    #         vqpu_ids = [1, 2, 3, 16], 
+    #     )
+    #     serialized = serializer.serialize(myflow)
+    #     print('Check if serialization using custom serializer works')
+    #     self.assertEqual(serialized, myflow.to_dict())
 
-    def test_serialize_on_arbitrary_class(self):
-        frame = inspect.currentframe()
-        # Get the function name
-        function_name = frame.f_code.co_name
-        # Get the line number
-        line_number = frame.f_lineno
-        print(f"Function name: {function_name}, Line number: {line_number}")
-        serializer = HybridQuantumWorkflowSerializer()
-        obj  = SillyClass(x = 100)
-        print('Check if serialization using custom serializer works on simply class not explicitly listed')
-        # serialized = serializer.serialize(obj)
-        x : float = 1.0
-        y = { 'foo' : [1,2,4,5], 'bar': 'what?'}
-        serialized = serializer.serialize(y)
-        deserialzed = serializer.deserialize(serialized)
-        self.assertEqual(y, deserialzed)
+    # def test_serialize_on_arbitrary_class(self):
+    #     frame = inspect.currentframe()
+    #     # Get the function name
+    #     function_name = frame.f_code.co_name
+    #     # Get the line number
+    #     line_number = frame.f_lineno
+    #     print(f"Function name: {function_name}, Line number: {line_number}")
+    #     serializer = HybridQuantumWorkflowSerializer()
+    #     obj  = SillyClass(x = 100)
+    #     print('Check if serialization using custom serializer works on simply class not explicitly listed')
+    #     # serialized = serializer.serialize(obj)
+    #     x : float = 1.0
+    #     y = { 'foo' : [1,2,4,5], 'bar': 'what?'}
+    #     serialized = serializer.serialize(y)
+    #     deserialzed = serializer.deserialize(serialized)
+    #     self.assertEqual(y, deserialzed)
 
-    def test_deserialize(self):
-        frame = inspect.currentframe()
-        # Get the function name
-        function_name = frame.f_code.co_name
-        # Get the line number
-        line_number = frame.f_lineno
-        print(f"Function name: {function_name}, Line number: {line_number}")
-        serializer = HybridQuantumWorkflowSerializer()
-        myflow = HybridQuantumWorkflowBase(
-            cluster = 'ella-qb', 
-            vqpu_ids = [1, 2, 3, 16], 
-        )
-        data = myflow.to_dict()  # Use a dictionary representing the serialized form
-        obj = serializer.deserialize(data)
-        print('Check if deserialization using custom serializer works')
-        self.assertIsInstance(obj, HybridQuantumWorkflowBase)
+    # def test_deserialize(self):
+    #     frame = inspect.currentframe()
+    #     # Get the function name
+    #     function_name = frame.f_code.co_name
+    #     # Get the line number
+    #     line_number = frame.f_lineno
+    #     print(f"Function name: {function_name}, Line number: {line_number}")
+    #     serializer = HybridQuantumWorkflowSerializer()
+    #     myflow = HybridQuantumWorkflowBase(
+    #         cluster = 'ella-qb', 
+    #         vqpu_ids = [1, 2, 3, 16], 
+    #     )
+    #     data = myflow.to_dict()  # Use a dictionary representing the serialized form
+    #     obj = serializer.deserialize(data)
+    #     print('Check if deserialization using custom serializer works')
+    #     self.assertIsInstance(obj, HybridQuantumWorkflowBase)
 
     def test_flowwithlocalclass(self):
         frame = inspect.currentframe()
@@ -205,18 +243,28 @@ class TestCustomSerializer(unittest.TestCase):
         # Get the line number
         line_number = frame.f_lineno
         print(f"Function name: {function_name}, Line number: {line_number}")
-        serializer = HybridQuantumWorkflowSerializer()
+        # serializer = HybridQuantumWorkflowSerializer()
         myflow = HybridQuantumWorkflowBase(
             cluster = 'ella-qb', 
             vqpu_ids = [1, 2, 3, 16], 
         )
-        # asyncio.run(launch_vqpu_workflow.with_options(
-        #     task_runner = myflow.taskrunners['vqpu'],
-        #     result_serializer=HybridQuantumWorkflowSerializer(),
-        #     )(myqpuworkflow=myflow, vqpu_id = 1, walltime = 400))
         asyncio.run(launch_vqpu_workflow.with_options(
             task_runner = myflow.gettaskrunner('vqpu'),
-            result_serializer=HybridQuantumWorkflowSerializer(),
+            )(myqpuworkflow=myflow, vqpu_id = 1, walltime = 10))
+
+    def test_multivqpu_flow(self):
+        frame = inspect.currentframe()
+        # Get the function name
+        function_name = frame.f_code.co_name
+        # Get the line number
+        line_number = frame.f_lineno
+        print(f"Function name: {function_name}, Line number: {line_number}")
+        myflow = HybridQuantumWorkflowBase(
+            cluster = 'ella-qb', 
+            vqpu_ids = [1, 2, 3, 16], 
+        )
+        asyncio.run(launch_vqpu_workflow.with_options(
+            task_runner = myflow.gettaskrunner('vqpu'),
             )(myqpuworkflow=myflow, vqpu_id = 1, walltime = 10))
 
 if __name__ == '__main__':
